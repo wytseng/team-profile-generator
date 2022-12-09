0e2cfc7a32c0d4ac467d56e6b6d8b384858d12e1
Steph T <wytseng@uci.edu>
2022-12-08 22:53:41 -0800

    clean up code and add comments to all files

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 const generatePage = require('./src/generate');
 
+// Declare object container with roles as keys to store furture data
 const teamMembers = {
   manager: '',
   engineers: [],
   interns: []
 }; 
+// Empty array to keep track of entered ID
 const storedId = [];
 
+// function that returns an array of questions based on different roles
 const generatePrompt = (role) => {
   const questions = [
     {
@@ -117,7 +120,9 @@ const generatePrompt = (role) => {
   return questions;
 }
 
+// triggers the app
 function app() {
+  // creates a manager obj with given response and saves that to teamMembers
   function createManager() {
     console.log(chalk.gray("------------------"));
     console.log(chalk.blueBright.bold("Welcome to the Team Generator! \nEnter your team member information and we will generate a page with your team's about for you!"));
@@ -129,12 +134,14 @@ function app() {
         const manager = new Manager(name.trim(), id.trim(), email.trim(), officeNum.trim());
         teamMembers.manager = manager;
         storedId.push(id);
-        console.log(teamMembers.manager.officeNumber);
         console.log(chalk.magenta(`Team manager - ${manager.name} - added to record!`));
         console.log(chalk.gray("------------------"));
+        // prompts for next action
         addMembers();
       }) 
   }
+
+  // function that prompts for user action and responds different to each input
   function addMembers() {
     inquirer
       .prompt([
@@ -164,6 +171,7 @@ function app() {
       })
   }
 
+  // creates engineer object from given response and add to teamMembers
   function addEngineer() {
     console.log(chalk.gray('------------------'));
     console.log(chalk.magenta('Adding an Engineer to the team'));
@@ -173,13 +181,14 @@ function app() {
         const engineer = new Engineer(name.trim(), id.trim(), email.trim(), github.trim());
         teamMembers.engineers.push(engineer);
         storedId.push(id);
-        console.log(teamMembers.engineers[0].github);
         console.log(chalk.magenta(`Engineer - ${engineer.name} - added to record!`));
         console.log(chalk.gray("------------------"));
+        // prompts for next action
         addMembers();
       }) 
   }
 
+  // creates intern object from given response and add to teamMembers
   function addIntern() {
     console.log(chalk.gray('------------------'));
     console.log(chalk.magenta('Adding an Intern to the team'));
@@ -189,20 +198,21 @@ function app() {
         const intern = new Intern(name.trim(), id.trim(), email.trim(), school.trim());
         teamMembers.interns.push(intern);
         storedId.push(id);
-        console.log(teamMembers.interns[0].school);
         console.log(chalk.magenta(`Intern - ${intern.name} - added to record!`));
         console.log(chalk.gray("------------------"));
+        // prompts for next action
         addMembers();
       }) 
   }
 
+  // generates html with information in teamMembers and writes to ./dist/team.html
   function renderPage() {
     fs.writeFile(path.join('dist', 'team.html'), generatePage(teamMembers), 'utf8', (err) => {
       err ? console.error(err) : console.log(chalk.blueBright.bold("'About Team' page is generated!"))
     });
   }
+  // starts prompts
   createManager();
 }
 
-
-app()
+app();
