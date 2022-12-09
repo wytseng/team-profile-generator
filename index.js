3d3235b843c55e741a0d31d8c15e9a6191e060c5
Steph T <wytseng@uci.edu>
2022-12-08 20:57:39 -0800

    complete all question prompts and able to save entry in an array

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 const fs = require('fs');
+const path = require('path');
 const inquirer = require('inquirer');
-const { off } = require('process');
+const chalk = require('chalk');
 
 const teamMembers = []; 
 
@@ -13,7 +14,7 @@ const generatePrompt = (role) => {
   const questions = [
     {
       type: "input",
-      message: "What is the team manager's name?",
+      message: `What is the ${role}'s name?`,
       name: 'name',
       validate: (name) => {
         if (name.trim() !== '') {
@@ -24,7 +25,7 @@ const generatePrompt = (role) => {
     },
     {
       type: "input",
-      message: "What is the team manager's employee ID?",
+      message: `What is the ${role}'s employee ID?`,
       name: 'id',
       validate: (id) => {
         if (id.trim() === '') {
@@ -34,14 +35,14 @@ const generatePrompt = (role) => {
           if (Number(id) > 0) {
             return true;
           }
-          return "Employe ID has to be a positive integer.";
+          return "Employee ID has to be a positive integer.";
         }
         return "Employee ID has to be an integer.";
       }
     },
     {
       type: "input",
-      message: "What is the team manager's email?",
+      message: `What is the ${role}'s email?`,
       name: 'email',
       validate: (email) => {
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
@@ -74,19 +75,54 @@ const generatePrompt = (role) => {
         }
       );
       break;
+    case 'Engineer': 
+      questions.push(
+        {
+          type: "input",
+          message: "What is your Github username?",
+          name: 'github',
+          validate: (github) => {
+            if (/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i.test(github)) {
+              return true;
+            }
+            return "Invalid Github username. Try Again."
+          }
+        }
+      );
+      break;
+    case 'Intern': 
+      questions.push(
+        {
+          type: "input",
+          message: "What school are you attending?",
+          name: 'school',
+          validate: (school) => {
+            if (school.trim() !== '') {
+              return true;
+            }
+            return "School name cannot be empty.";
+          }
+        }
+      );
+      break;
   }
   return questions;
 }
 
 function app() {
   function createManager() {
-    console.log("Welcome to the Team Generator! \nEnter your team member information and we will generate a page with your team's about for you!");
+    console.log(chalk.gray("------------------"));
+    console.log(chalk.blueBright.bold("Welcome to the Team Generator! \nEnter your team member information and we will generate a page with your team's about for you!"));
+    console.log(chalk.gray("------------------"));
+    console.log(chalk.magenta("First, we need information on the team manager."));
     inquirer
       .prompt(generatePrompt('Manager'))
       .then(({name, id, email, officeNum}) => {
         const manager = new Manager(name, id, email, officeNum);
         teamMembers.push(manager);
-        console.log(manager);
+        console.log(teamMembers);
+        console.log(chalk.magenta(`Team manager - ${manager.name} - added to record!`));
+        console.log(chalk.gray("------------------"));
         addMembers();
       }) 
   }
@@ -95,7 +131,7 @@ function app() {
       .prompt([
         {
           type: 'list',
-          message: 'Would you like to add a team member?',
+          message: 'Would you like to add more team members?',
           name: 'action',
           choices: [
             "Add an Engineer",
@@ -104,28 +140,55 @@ function app() {
           ]
         }
       ])
-      .then((response) => {
-          switch(response) {
+      .then(({action}) => {
+          switch(action) {
             case 'Add an Engineer':
               addEngineer();
+              break;
             case 'Add an Intern':
               addIntern();
+              break;
             case 'Finish building my team':
               renderPage();
+              break;
           }
       })
   }
 
   function addEngineer() {
-    console.log('add an engineer');
+    console.log(chalk.gray('------------------'));
+    console.log(chalk.magenta('Adding an Engineer to the team'));
+    inquirer
+      .prompt(generatePrompt('Engineer'))
+      .then(({name, id, email, github}) => {
+        const engineer = new Engineer(name, id, email, github);
+        teamMembers.push(engineer);
+        console.log(teamMembers);
+        console.log(chalk.magenta(`Engineer - ${engineer.name} - added to record!`));
+        console.log(chalk.gray("------------------"));
+        addMembers();
+      }) 
   }
 
   function addIntern() {
-    console.log('add an intern');
+    console.log(chalk.gray('------------------'));
+    console.log(chalk.magenta('Adding an Intern to the team'));
+    inquirer
+      .prompt(generatePrompt('Intern'))
+      .then(({name, id, email, school}) => {
+        const intern = new Intern(name, id, email, school);
+        teamMembers.push(intern);
+        console.log(teamMembers);
+        console.log(chalk.magenta(`Intern - ${intern.name} - added to record!`));
+        console.log(chalk.gray("------------------"));
+        addMembers();
+      }) 
   }
 
   function renderPage() {
-    console.log('render page');
+    fs.writeFile(path.join('dist', 'team.html'), JSON.stringify(teamMembers), 'utf8', (err) => {
+      err ? console.error(err) : console.log(chalk.blueBright.bold("'About Team' page generated!"))
+    });
   }
 
   createManager();
